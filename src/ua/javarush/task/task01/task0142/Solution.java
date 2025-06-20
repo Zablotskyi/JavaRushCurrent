2ca8d350185b283f9c64ffe5cd258bb6306dd395
Zablotskyi <zablotskyivv@gmail.com>
2025-06-20 15:56:18 +0300

    started to perfomr the task0142 'Аміго дуже розумний'

---
+++ src/ua/javarush/task/task01/task0142/Solution.java
@@ -0,0 +1,11 @@
+package ua.javarush.task.task01.task0142;
+
+/* 
+Аміго дуже розумний
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+    }
+}
