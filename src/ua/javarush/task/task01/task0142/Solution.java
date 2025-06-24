38d5f8243eaa0fe92f6a2e04ea0bbdd07fa4761e
Zablotskyi <zablotskyivv@gmail.com>
2025-06-24 12:28:21 +0300

    finished the task0101 'Я думаю, бути програмістом - круто'

--- src/ua/javarush/task/task01/task0142/Solution.java
+++ src/ua/javarush/task/task01/task0142/Solution.java
@@ -7,5 +7,6 @@ package ua.javarush.task.task01.task0142;
 public class Solution {
     public static void main(String[] args) {
         //напишіть тут ваш код
+        System.out.println("Аміго дуже розумний");
     }
 }
