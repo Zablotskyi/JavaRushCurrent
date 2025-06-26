3e63c289a5505b0c245741a24f53c958fa11e647
Zablotskyi <zablotskyivv@gmail.com>
2025-06-26 11:50:37 +0300

    started to perofrm the task0105 'Оголошуємо зміннні'

---
+++ src/ua/javarush/task/task01/task0105/Solution.java
@@ -0,0 +1,11 @@
+package ua.javarush.task.task01.task0105;
+
+/* 
+Оголошуємо змінні
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+    }
+}
