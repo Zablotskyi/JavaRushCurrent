be829ae1834ce5ba8d3363e1d320d34ef6972931
Zablotskyi <zablotskyivv@gmail.com>
2025-06-26 11:58:18 +0300

    task0105 completed

--- src/ua/javarush/task/task01/task0105/Solution.java
+++ src/ua/javarush/task/task01/task0105/Solution.java
@@ -7,5 +7,8 @@ package ua.javarush.task.task01.task0105;
 public class Solution {
     public static void main(String[] args) {
         //напишіть тут ваш код
+        String name;
+        int age;
+        String city;
     }
 }
