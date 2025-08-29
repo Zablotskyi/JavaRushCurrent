9b1aff6c2ea816cc99c71aa8fc744c5d09c17139
Zablotskyi <zablotskyivv@gmail.com>
2025-08-29 17:47:49 +0300

    completed the task0103 'Мій юний друг'

--- src/ua/javarush/task/task01/task0103/Solution.java
+++ src/ua/javarush/task/task01/task0103/Solution.java
@@ -7,5 +7,9 @@ package ua.javarush.task.task01.task0103;
 public class Solution {
     public static void main(String[] args) {
         //напишіть тут ваш код
+        int currentYear = 3126;
+        int myFriendYear = 8;
+        int myFriendBirthdayIs = currentYear - myFriendYear;
+        System.out.println(myFriendBirthdayIs);
     }
 }
