5038d40c3e4da41c145c3d20ea34bbb7730b18ba
Zablotskyi <zablotskyivv@gmail.com>
2025-08-29 17:44:08 +0300

    completed the task0109 'Оголошення та ініціалізація змінних'

--- src/ua/javarush/task/task01/task0109/Solution.java
+++ src/ua/javarush/task/task01/task0109/Solution.java
@@ -7,5 +7,7 @@ package ua.javarush.task.task01.task0109;
 public class Solution {
     public static void main(String[] args) {
         //напишіть тут ваш код
+        int a = 13;
+        int b = 666;
     }
 }
