b62397f155da7bd8b38a5502009ca767306dc0ed
Zablotskyi <zablotskyivv@gmail.com>
2025-08-29 17:45:15 +0300

    started to perform the task0103 'Мій юний друг'

---
+++ src/ua/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,11 @@
+package ua.javarush.task.task01.task0103;
+
+/* 
+Мій юний друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+    }
+}
