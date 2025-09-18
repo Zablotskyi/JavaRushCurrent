401dc1e14979fbb79456d76a2091e2ba71b0d3df
Zablotskyi <zablotskyivv@gmail.com>
2025-09-18 17:17:28 +0300

    started to perform the task0204 '9 - це перевернута цифра 6'

---
+++ src/ua/javarush/task/pro/task02/task0204/Solution.java
@@ -0,0 +1,17 @@
+package ua.javarush.task.pro.task02.task0204;
+
+/* 
+9 — це перевернута цифра 6
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int six = 6;
+
+        //напишіть тут ваш код
+
+
+        System.out.println(six);
+    }
+}
