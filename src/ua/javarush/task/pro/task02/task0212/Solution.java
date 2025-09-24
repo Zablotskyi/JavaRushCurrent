ab96bcfebcf104c327a2892d3ad828a1fbc8042a
Zablotskyi <zablotskyivv@gmail.com>
2025-09-24 22:03:07 +0300

    started to perform the task0212 'Прошу регістр на мене не підвищувати!'

---
+++ src/ua/javarush/task/pro/task02/task0212/Solution.java
@@ -0,0 +1,18 @@
+package ua.javarush.task.pro.task02.task0212;
+
+/* 
+Прошу регістр на мене не підвищувати!
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String caps = "if I type in caps ";
+        String usa = "сша";
+
+        //напишіть тут ваш код
+        System.out.println(usa);
+        System.out.println("Вінні-Пух");
+        System.out.println(caps + "they know I mean business");
+    }
+}
