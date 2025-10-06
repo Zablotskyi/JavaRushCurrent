bed2ddbb6f9a30e99ccd90fc861d4983cbbc233f
Zablotskyi <zablotskyivv@gmail.com>
2025-10-06 17:08:18 +0300

    completed the task0215 'Читання чисел'

--- src/ua/javarush/task/pro/task02/task0215/Solution.java
+++ src/ua/javarush/task/pro/task02/task0215/Solution.java
@@ -10,6 +10,11 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишіть тут ваш код
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        System.out.println((a + b + c) / 3);
 
     }
 }
