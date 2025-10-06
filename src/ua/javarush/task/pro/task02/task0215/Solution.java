f130be6ab1aaad1576fc259d09241fb30b9e4bf0
Zablotskyi <zablotskyivv@gmail.com>
2025-10-06 17:02:51 +0300

    started to perform the task0215 'Читання чисел'

---
+++ src/ua/javarush/task/pro/task02/task0215/Solution.java
@@ -0,0 +1,15 @@
+package ua.javarush.task.pro.task02.task0215;
+
+import java.util.Scanner;
+
+/* 
+Читання чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+
+    }
+}
