4bd792dcff9621267f0fc1e9c7fada039258bb35
Zablotskyi <zablotskyivv@gmail.com>
2025-10-06 17:09:23 +0300

    started to perform the task0214 'Читання і перетворення рядків'

---
+++ src/ua/javarush/task/pro/task02/task0214/Solution.java
@@ -0,0 +1,15 @@
+package ua.javarush.task.pro.task02.task0214;
+
+import java.util.Scanner;
+
+/* 
+Читання і перетворення рядків
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+
+    }
+}
