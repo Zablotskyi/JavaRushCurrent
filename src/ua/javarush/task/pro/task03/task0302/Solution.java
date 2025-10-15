9e2e1a3289a97c8c47af40e8872eb47f9625b9a2
Zablotskyi <zablotskyivv@gmail.com>
2025-10-15 20:26:58 +0300

    started to perform the task 'Призовна кампанія'

---
+++ src/ua/javarush/task/pro/task03/task0302/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.pro.task03.task0302;
+
+import java.util.Scanner;
+
+/* 
+Призовна кампанія
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String militaryCommissar = ", з'явіться до військкомату";
+        //напишіть тут ваш код
+    }
+}
