b23630626007fabdd6dd507284d857ab9ee6cd48
Zablotskyi <zablotskyivv@gmail.com>
2025-10-16 22:09:02 +0300

    completed the task0303 'Школа чи інститут'

--- src/ua/javarush/task/pro/task03/task0303/Solution.java
+++ src/ua/javarush/task/pro/task03/task0303/Solution.java
@@ -10,10 +10,11 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int age = scanner.nextInt();
-        if (age < 18)
+        if (age < 18) {
             if (age >= 6)
                 System.out.println("треба ходити до школи");
-        else
+        } else
             System.out.println("час до інституту");
+
     }
 }
