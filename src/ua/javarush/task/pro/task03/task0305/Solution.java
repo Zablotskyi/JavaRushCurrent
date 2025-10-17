d59f033535f2a7e86ad175b9da89460d28c191d4
Zablotskyi <zablotskyivv@gmail.com>
2025-10-17 17:02:47 +0300

    started to perform the task0305 'Три числа'

---
+++ src/ua/javarush/task/pro/task03/task0305/Solution.java
@@ -0,0 +1,13 @@
+package ua.javarush.task.pro.task03.task0305;
+
+import java.util.Scanner;
+
+/* 
+Три числа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+    }
+}
