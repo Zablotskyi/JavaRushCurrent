361c9e5e5d817aef479bda4a20de965d88870d32
Zablotskyi <zablotskyivv@gmail.com>
2025-10-17 17:18:51 +0300

    completed the task0305 'Три числа'

--- src/ua/javarush/task/pro/task03/task0305/Solution.java
+++ src/ua/javarush/task/pro/task03/task0305/Solution.java
@@ -9,5 +9,23 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         //напишіть тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if (a == b && b == c) {
+            System.out.println(a + " " + b + " " + c);
+            return;
+        }
+        if (a == b) {
+            System.out.println(a + " " + b);
+        }
+        if (b == c) {
+            System.out.println(b + " " + c);
+        }
+        if (a == c) {
+            System.out.println(a + " " + c);
+        }
     }
 }
