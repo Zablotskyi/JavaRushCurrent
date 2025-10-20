b90e04f07f535f0b833afbb500eaf0b073367390
Zablotskyi <zablotskyivv@gmail.com>
2025-10-20 22:27:16 +0300

    started to perform the task0307 'Працювати чи не працювати - отаке питання'

---
+++ src/ua/javarush/task/pro/task03/task0307/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.pro.task03.task0307;
+
+import java.util.Scanner;
+
+/* 
+Працювати чи не працювати — отаке питання
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+        System.out.println("можна не працювати");
+    }
+}
