5f9a02fa3818314c5189b4bfb9656b822dd87f68
Zablotskyi <zablotskyivv@gmail.com>
2025-10-20 22:33:00 +0300

    completed the task0307 'Працювати чи не працювати -отаке питання'

--- src/ua/javarush/task/pro/task03/task0307/Solution.java
+++ src/ua/javarush/task/pro/task03/task0307/Solution.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         //напишіть тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        if (age < 20 || age > 60)
         System.out.println("можна не працювати");
     }
 }
