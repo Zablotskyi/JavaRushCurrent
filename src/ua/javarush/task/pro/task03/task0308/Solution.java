6e30b93ad9df58bf53482a152e751cbb1a8a5737
Zablotskyi <zablotskyivv@gmail.com>
2025-10-21 19:53:12 +0300

    started to perform the task0308 'Координатні чверті'

---
+++ src/ua/javarush/task/pro/task03/task0308/Solution.java
@@ -0,0 +1,13 @@
+package ua.javarush.task.pro.task03.task0308;
+
+import java.util.Scanner;
+
+/* 
+Координатні чверті
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+    }
+}
