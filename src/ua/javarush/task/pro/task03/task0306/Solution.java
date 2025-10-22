43ce15774c56439056842042a6d2a269bbdfcd18
Zablotskyi <zablotskyivv@gmail.com>
2025-10-22 20:48:03 +0300

    started to perform the task0306 'Трикутник'

---
+++ src/ua/javarush/task/pro/task03/task0306/Solution.java
@@ -0,0 +1,16 @@
+package ua.javarush.task.pro.task03.task0306;
+
+import java.util.Scanner;
+
+/* 
+Трикутник
+*/
+
+public class Solution {
+    private static final String TRIANGLE_EXISTS = "трикутник існує";
+    private static final String TRIANGLE_NOT_EXISTS = "трикутник не існує";
+
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+    }
+}
