461df69dc09dcf4b6d885417194dde4904deed80
Zablotskyi <zablotskyivv@gmail.com>
2025-10-22 10:21:08 +0300

    completed the task0308 'Координатні чверті'

--- src/ua/javarush/task/pro/task03/task0308/Solution.java
+++ src/ua/javarush/task/pro/task03/task0308/Solution.java
@@ -9,5 +9,16 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         //напишіть тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int coordinateX = scanner.nextInt();
+        int coordinateY = scanner.nextInt();
+        if (coordinateX > 0 && coordinateY > 0)
+            System.out.println("1");
+        if (coordinateX < 0 && coordinateY > 0)
+            System.out.println("2");
+        if (coordinateX < 0 && coordinateY < 0)
+            System.out.println("3");
+        if (coordinateX > 0 && coordinateY < 0)
+            System.out.println("4");
     }
 }
