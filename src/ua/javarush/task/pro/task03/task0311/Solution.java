103012f0b7943c71bcc98d098038a2b8f0ded31f
Zablotskyi <zablotskyivv@gmail.com>
2025-10-25 12:20:35 +0300

    started to perform the task0311 'Висока точність'

---
+++ src/ua/javarush/task/pro/task03/task0311/Solution.java
@@ -0,0 +1,16 @@
+package ua.javarush.task.pro.task03.task0311;
+
+import java.util.Scanner;
+
+/* 
+Висока точність
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+        System.out.println("числа рівні");
+        //напишіть тут ваш код
+        System.out.println("числа не рівні");
+    }
+}
