81ccf9fcb293e282df12eec4dea863867c04bafa
Zablotskyi <zablotskyivv@gmail.com>
2025-10-26 15:06:13 +0200

    completed the task0312 'Порівняймо рядки'

--- src/ua/javarush/task/pro/task03/task0312/Solution.java
+++ src/ua/javarush/task/pro/task03/task0312/Solution.java
@@ -9,8 +9,13 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         //напишіть тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String firstString = scanner.nextLine();
+        String secondString = scanner.nextLine();
+        if (firstString.equals(secondString))
         System.out.println("рядки однакові");
         //напишіть тут ваш код
+        if (!firstString.equals(secondString))
         System.out.println("рядки різні");
     }
 }
