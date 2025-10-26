50dba107a3958774d018ee88f2b35fc2927cc468
Zablotskyi <zablotskyivv@gmail.com>
2025-10-26 15:18:53 +0200

    started to perform the taks0314 'Поламана клавіатура'

---
+++ src/ua/javarush/task/pro/task03/task0314/Solution.java
@@ -0,0 +1,18 @@
+package ua.javarush.task.pro.task03.task0314;
+
+import java.util.Scanner;
+
+/* 
+Поламана клавіатура
+*/
+
+public class Solution {
+    public static String secret = "AmIGo";
+
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+        System.out.println("доступ дозволено");
+        //напишіть тут ваш код
+        System.out.println("доступ заборонено");
+    }
+}
