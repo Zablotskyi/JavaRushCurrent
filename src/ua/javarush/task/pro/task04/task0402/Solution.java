e047870cd1fde4801975b75e841ee4995e09d509
Zablotskyi <zablotskyivv@gmail.com>
2025-10-27 21:26:15 +0200

    started to perform the task0402 'Усі люблять Мамбу'

---
+++ src/ua/javarush/task/pro/task04/task0402/Solution.java
@@ -0,0 +1,15 @@
+package ua.javarush.task.pro.task04.task0402;
+
+import java.util.Scanner;
+
+/* 
+Усі люблять Мамбу
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String text = " любить мене.";
+        //напишіть тут ваш код
+
+    }
+}
