cf3c492d97ca46aab2f9090b95c2fe906edd7347
Zablotskyi <zablotskyivv@gmail.com>
2025-10-27 21:41:08 +0200

    completed the task0404 'Заповнений прямокутник'

--- src/ua/javarush/task/pro/task04/task0404/Solution.java
+++ src/ua/javarush/task/pro/task04/task0404/Solution.java
@@ -7,6 +7,17 @@ package ua.javarush.task.pro.task04.task0404;
 public class Solution {
     public static void main(String[] args) {
         //напишіть тут ваш код
+        int vertical = 5;
 
+        while (vertical > 0) {
+            int horizontal = 10;
+
+            while (horizontal > 0) {
+                System.out.print("Q");
+                horizontal--;
+            }
+            System.out.println();
+            vertical--;
+        }
     }
 }
