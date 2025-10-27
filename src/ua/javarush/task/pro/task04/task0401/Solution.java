7a3a0cefe91e26b2e6b31aac9ee3bc97a0b36547
Zablotskyi <zablotskyivv@gmail.com>
2025-10-27 17:38:07 +0200

    completed the task0401 'Я ніколи не працюватиму за дрібняки'

--- src/ua/javarush/task/pro/task04/task0401/Solution.java
+++ src/ua/javarush/task/pro/task04/task0401/Solution.java
@@ -8,6 +8,11 @@ public class Solution {
     public static void main(String[] args) {
         String quote = "Я ніколи не працюватиму за дрібняки. Аміго";
         //напишіть тут ваш код
+        int counter = 100;
 
+        while (counter > 0) {
+            System.out.println("Я ніколи не працюватиму за дрібняки. Аміго");
+            counter--;
+        }
     }
 }
