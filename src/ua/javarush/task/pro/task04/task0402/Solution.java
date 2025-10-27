cc1c78ca3013d566dab0e51f4dd5af07f43dc8fb
Zablotskyi <zablotskyivv@gmail.com>
2025-10-27 21:27:29 +0200

    completed the task0402 'Усі люблять Мамбу'

--- src/ua/javarush/task/pro/task04/task0402/Solution.java
+++ src/ua/javarush/task/pro/task04/task0402/Solution.java
@@ -10,6 +10,13 @@ public class Solution {
     public static void main(String[] args) {
         String text = " любить мене.";
         //напишіть тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int counter = 10;
 
+        while (counter > 0) {
+            System.out.println(name + text);
+            counter--;
+        }
     }
 }
