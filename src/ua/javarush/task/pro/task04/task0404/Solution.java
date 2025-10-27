b2934c76a903010927315f1d0626cda284d733af
Zablotskyi <zablotskyivv@gmail.com>
2025-10-27 21:29:29 +0200

    started to perform the task0404 'Заповнений прямокутник'

---
+++ src/ua/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,12 @@
+package ua.javarush.task.pro.task04.task0404;
+
+/* 
+Заповнений прямокутник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+
+    }
+}
