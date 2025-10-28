a6fea564cec6661358313e2d3b2202c062c6dfe5
Zablotskyi <zablotskyivv@gmail.com>
2025-10-28 20:25:19 +0200

    started to perform the task0403 'Підсумовування'

---
+++ src/ua/javarush/task/pro/task04/task0403/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.pro.task04.task0403;
+
+import java.util.Scanner;
+
+/* 
+Підсумовування
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+
+    }
+}
