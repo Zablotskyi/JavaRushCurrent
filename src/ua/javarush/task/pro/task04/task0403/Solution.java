beb355a5f36a31473ab4ef6409abf1c82357c312
Zablotskyi <zablotskyivv@gmail.com>
2025-10-28 20:57:32 +0200

    completed the task0403 'Підсумовування'

--- src/ua/javarush/task/pro/task04/task0403/Solution.java
+++ src/ua/javarush/task/pro/task04/task0403/Solution.java
@@ -9,6 +9,16 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         //напишіть тут ваш код
+        boolean isExit = false;
+        Scanner scanner = new Scanner(System.in);
+        String s = "";
+        int number = 0;
 
+        number = Integer.parseInt(scanner.nextLine());
+        while (scanner.hasNextInt()) {
+            number = number + Integer.parseInt(s = scanner.nextLine());
+            isExit = s.equals("ENTER");
+        }
+        System.out.println(number);
     }
 }
