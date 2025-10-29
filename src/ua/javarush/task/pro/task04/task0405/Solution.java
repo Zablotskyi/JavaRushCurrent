6168003b455c9b4933ca5fca2e77060e7620588a
Zablotskyi <zablotskyivv@gmail.com>
2025-10-29 20:03:57 +0200

    started to perform the task0405 'Незаповнений прямокутник'

---
+++ src/ua/javarush/task/pro/task04/task0405/Solution.java
@@ -0,0 +1,12 @@
+package ua.javarush.task.pro.task04.task0405;
+
+/* 
+Незаповнений прямокутник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+
+    }
+}
