981fc29ff2374017a0e32394db922fedff5213e5
Zablotskyi <zablotskyivv@gmail.com>
2025-10-30 22:19:01 +0200

    completed the task0405 'Незаповнений прямокутник'

--- src/ua/javarush/task/pro/task04/task0405/Solution.java
+++ src/ua/javarush/task/pro/task04/task0405/Solution.java
@@ -7,6 +7,28 @@ package ua.javarush.task.pro.task04.task0405;
 public class Solution {
     public static void main(String[] args) {
         //напишіть тут ваш код
+        int horizontal = 20;
+        int vertical = 10;
 
+        while (vertical > 0) {
+            if (vertical == 10 || vertical == 1) {
+                while (horizontal > 0) {
+                    System.out.print("Б");
+                    horizontal--;
+                }
+            } else {
+                while (horizontal > 0) {
+                    if (horizontal == 20 || horizontal == 1) {
+                        System.out.print("Б");
+                    } else {
+                        System.out.print(" ");
+                    }
+                    horizontal--;
+                }
+            }
+            System.out.println();
+            horizontal = 20;
+            vertical--;
+        }
     }
 }
