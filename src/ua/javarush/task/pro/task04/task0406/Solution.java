bd88e35a83a9c3dbd4b998a1a8d24212b30a1e3e
Zablotskyi <zablotskyivv@gmail.com>
2025-10-31 17:28:23 +0200

    completed the task 'Показуємо, що отримуємо'

--- src/ua/javarush/task/pro/task04/task0406/Solution.java
+++ src/ua/javarush/task/pro/task04/task0406/Solution.java
@@ -9,6 +9,15 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         //напишіть тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String str;
+
+        while (true) {
+            str = scanner.nextLine();
+            if (str.equals("enough"))
+                break;
+            System.out.println(str);
+        }
 
     }
 }
