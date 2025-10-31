12cff4e57e1d851cdcaba1070c3f3aa08ab9019b
Zablotskyi <zablotskyivv@gmail.com>
2025-10-31 17:24:17 +0200

    started to perform the task0406 'Показуємо, що отримуємо'

---
+++ src/ua/javarush/task/pro/task04/task0406/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.pro.task04.task0406;
+
+import java.util.Scanner;
+
+/* 
+Показуємо, що отримуємо
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+
+    }
+}
