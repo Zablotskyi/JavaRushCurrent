a6adb65b06f6318d5bffde6365f92228cd460325
Zablotskyi <zablotskyivv@gmail.com>
2025-11-02 18:03:48 +0200

    started to perform the task0408 'Мінімум серед уведених чисел'

---
+++ src/ua/javarush/task/pro/task04/task0408/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.pro.task04.task0408;
+
+import java.util.Scanner;
+
+/* 
+Максимум серед уведених чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+
+    }
+}
