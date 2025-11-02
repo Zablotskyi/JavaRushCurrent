ddb4b19f8a7a7cc6d74055962a5d15f3dab6cbd9
Zablotskyi <zablotskyivv@gmail.com>
2025-11-02 18:10:06 +0200

    completed the task0408 'Максимум серед уведених чисел'

--- src/ua/javarush/task/pro/task04/task0408/Solution.java
+++ src/ua/javarush/task/pro/task04/task0408/Solution.java
@@ -9,6 +9,16 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         //напишіть тут ваш код
+        int max = Integer.MIN_VALUE;
+        Scanner scanner = new Scanner(System.in);
 
+        while (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+            if (number % 2 == 0) {
+                if (number > max)
+                    max = number;
+            }
+        }
+        System.out.println(max);
     }
 }
