4fca46237bbdb841469fca2ebca1e125fb3954ff
Zablotskyi <zablotskyivv@gmail.com>
2025-11-05 16:30:40 +0200

    started to perform the task0410 'Друге мінімальне число серед уведених'

---
+++ src/ua/javarush/task/pro/task04/task0410/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.pro.task04.task0410;
+
+import java.util.Scanner;
+
+/* 
+Друге мінімальне число серед уведених
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+
+    }
+}
