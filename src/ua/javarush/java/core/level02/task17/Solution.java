31aef9216e5ef289e7b08e622e6156a3c352f373
Zablotskyi <zablotskyivv@gmail.com>
2026-06-06 17:51:28 +0300

    started to perform the task17

---
+++ src/ua/javarush/java/core/level02/task17/Solution.java
@@ -0,0 +1,16 @@
+package ua.javarush.java.core.level02.task17;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Створюємо Scanner для зчитування даних з клавіатури
+
+
+        // Зчитуємо один рядок як пароль
+
+
+        // Виводимо введений пароль на екран
+
+    }
+}
