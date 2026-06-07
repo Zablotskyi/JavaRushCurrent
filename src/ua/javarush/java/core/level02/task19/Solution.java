b276cedb6590a02a6776413b8c442e6b81b27994
Zablotskyi <zablotskyivv@gmail.com>
2026-06-07 11:43:50 +0300

    started to perform the task19 'Підрахунок загальної вартості покупок'

---
+++ src/ua/javarush/java/core/level02/task19/Solution.java
@@ -0,0 +1,16 @@
+package ua.javarush.java.core.level02.task19;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Створюємо Scanner для зчитування даних з клавіатури
+
+
+        // Зчитуємо ціни двох товарів як цілі числа
+
+
+        // Додаємо та виводимо суму
+
+    }
+}
