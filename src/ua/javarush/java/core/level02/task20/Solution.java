9eec6616c425f7fc2d03fb5b6f431b8644a66e0d
Zablotskyi <zablotskyivv@gmail.com>
2026-06-07 17:44:02 +0300

    task20 is done

--- src/ua/javarush/java/core/level02/task20/Solution.java
+++ src/ua/javarush/java/core/level02/task20/Solution.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Створюємо об’єкт Scanner для зчитування даних з консолі
-
+        Scanner scanner = new Scanner(System.in);
 
         // Зчитуємо ім’я користувача (рядок)
-
+        System.out.println("Введіть своє їм'я: ");
+        String name = scanner.nextLine();
 
         // Зчитуємо вік користувача (ціле число)
-
+        System.out.println("Введіть свій вік: ");
+        int age = scanner.nextInt();
 
         // Виводимо відформатовану візитну картку
+        System.out.println("Мене звати " + name + ", мені " + age + " років.");
 
     }
 }
