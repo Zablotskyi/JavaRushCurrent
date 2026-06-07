151734d773828421344495ba34b2ac620e3e3162
Zablotskyi <zablotskyivv@gmail.com>
2026-06-07 11:33:01 +0300

    task18 is done

--- src/ua/javarush/java/core/level02/task18/Solution.java
+++ src/ua/javarush/java/core/level02/task18/Solution.java
@@ -5,16 +5,21 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Створюємо об’єкт Scanner для читання даних з клавіатури
-
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        int age;
 
         // Запитуємо та зчитуємо ім’я гравця
-
+        System.out.println("Введіть ім'я гравця: ");
+        name = scanner.nextLine();
 
         // Запитуємо та зчитуємо вік гравця
-
+        System.out.println("Введіть вік гравця: ");
+        age = scanner.nextInt();
 
         // Спочатку виводимо вік, потім ім’я, кожне з нового рядка
-
+        System.out.println(age);
+        System.out.println(name);
 
     }
 }
