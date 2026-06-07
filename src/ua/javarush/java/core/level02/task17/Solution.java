7a3f1b3201512a6fda4b43914bc5e19e8acf5ffe
Zablotskyi <zablotskyivv@gmail.com>
2026-06-07 11:27:23 +0300

    started to perform the task18 'Реєстрація новго гравця'

--- src/ua/javarush/java/core/level02/task17/Solution.java
+++ src/ua/javarush/java/core/level02/task17/Solution.java
@@ -1,16 +1,18 @@
 package ua.javarush.java.core.level02.task17;
 
+
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         // Створюємо Scanner для зчитування даних з клавіатури
-
+        Scanner scanner = new Scanner(System.in);
 
         // Зчитуємо один рядок як пароль
+        String pass = scanner.nextLine();
 
 
         // Виводимо введений пароль на екран
-
+        System.out.println(pass);
     }
 }
---
+++ src/ua/javarush/java/core/level02/task18/Solution.java
@@ -0,0 +1,20 @@
+package ua.javarush.java.core.level02.task18;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Створюємо об’єкт Scanner для читання даних з клавіатури
+
+
+        // Запитуємо та зчитуємо ім’я гравця
+
+
+        // Запитуємо та зчитуємо вік гравця
+
+
+        // Спочатку виводимо вік, потім ім’я, кожне з нового рядка
+
+
+    }
+}
