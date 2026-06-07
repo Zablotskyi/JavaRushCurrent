3eaea2dde7ee93108508e1daa0f362a21d4ed36e
Zablotskyi <zablotskyivv@gmail.com>
2026-06-07 11:44:38 +0300

    the task19 is done

--- src/ua/javarush/java/core/level02/task19/Solution.java
+++ src/ua/javarush/java/core/level02/task19/Solution.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Створюємо Scanner для зчитування даних з клавіатури
-
+        Scanner scanner = new Scanner(System.in);
 
         // Зчитуємо ціни двох товарів як цілі числа
-
+        System.out.println("Введіть ціну першого товару");
+        int firstPrice = scanner.nextInt();
+        System.out.println("Введіть ціну другого товару");
+        int secondPrice = scanner.nextInt();
 
         // Додаємо та виводимо суму
+        int sum = firstPrice + secondPrice;
+        System.out.println("Загальна сума по всім товарам: " + sum);
 
     }
 }
