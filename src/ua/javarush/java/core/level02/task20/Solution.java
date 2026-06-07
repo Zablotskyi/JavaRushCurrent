58a06d3ac7a4cf9c0a1bddf10cbcf80ba0a0e56c
Zablotskyi <zablotskyivv@gmail.com>
2026-06-07 17:43:29 +0300

    started to perform the task20 'Створення візитної картки'

---
+++ src/ua/javarush/java/core/level02/task20/Solution.java
@@ -0,0 +1,19 @@
+package ua.javarush.java.core.level02.task20;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Створюємо об’єкт Scanner для зчитування даних з консолі
+
+
+        // Зчитуємо ім’я користувача (рядок)
+
+
+        // Зчитуємо вік користувача (ціле число)
+
+
+        // Виводимо відформатовану візитну картку
+
+    }
+}
