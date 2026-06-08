e02f77e3f2be961745264492bd228d66ef927acf
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-08 14:29:20 +0300

    the task04 is done

--- src/ua/javarush/java/core/level03/task04/Solution.java
+++ src/ua/javarush/java/core/level03/task04/Solution.java
@@ -3,18 +3,18 @@ package ua.javarush.java.core.level03.task04;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо змінну currentHour і надаємо їй значення 20
-
+        int currentHour = 20;
 
         // Використовуємо оператор if-else if-else для вибору потрібного привітання
-        if (/* умова */) {
+        if (currentHour < 12) {
             // Якщо поточний час менше 12, вважаємо, що це ранок
-
-        } else if (/* умова */) {
+            System.out.println("Доброго ранку!");
+        } else if (currentHour >= 12 && currentHour <= 18) {
             // Якщо поточний час від 12 до 18 включно, вважаємо, що це день
-
+            System.out.println("Добрий день!");
         } else {
             // В усіх інших випадках вважаємо, що це вечір
-
+            System.out.println("Добрий вечір!");
         }
     }
 }
