7b74444bd5cba19e7ce32b58547d4d422b8e79b8
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-08 13:34:24 +0300

    the task03 is done

--- src/ua/javarush/java/core/level03/task03/Solution.java
+++ src/ua/javarush/java/core/level03/task03/Solution.java
@@ -3,12 +3,12 @@ package ua.javarush.java.core.level03.task03;
 public class Solution {
     public static void main(String[] args) {
         // Оголоси змінну userPassword і присвой їй значення "qwerty"
-
+        String userPassword = "qwerty";
 
         // Перевір, чи збігається пароль із очікуваним значенням "qwerty"
-        if (/* умова */) {
+        if (userPassword.equals("qwerty")) {
             // Якщо умова істинна, виведи повідомлення про успішний вхід
-
+            System.out.println("Вхід виконано успішно");
         }
         // Якщо пароль не збігається, блок else не використовується і програма нічого не виводить
     }
