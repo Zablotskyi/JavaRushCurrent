76636e96fe3bcba4ec8e17db6face46e82ab0cda
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-08 13:24:07 +0300

    task01 is done

--- src/ua/javarush/java/core/level03/task01/Solution.java
+++ src/ua/javarush/java/core/level03/task01/Solution.java
@@ -3,15 +3,15 @@ package ua.javarush.java.core.level03.task01;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо змінну visitorAge і одразу ініціалізуємо її значенням 17
-
+        int visitorAge = 17;
 
         // Використовуємо оператор if-else для перевірки: якщо вік менше 18
-        if (/* умова */) {
+        if (visitorAge < 18) {
             // У разі неповноліття виводимо повідомлення про заборону доступу
-
+            System.out.println("Доступ заборонено");
         } else {
             // Інакше — вітаємо відвідувача
-
+            System.out.println("Ласкаво просимо!");
         }
     }
 }
