96c1dd2570082e123d9c44202da39d9d5ea42bf2
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-09 15:52:17 +0300

    started to perform the task10 'Результат матчу'

---
+++ src/ua/javarush/java/core/level03/task10/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.java.core.level03.task10;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголошуємо та присвоюємо рахунок нашої команди й суперника
+
+
+        // weWon — результат логічного порівняння: true, якщо наш рахунок більший
+
+
+        // Виводимо значення змінної weWon на екран
+
+    }
+}
