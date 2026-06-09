fbfbe8a46071b332bd37cf1f1bd4846e92012a20
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-09 15:50:39 +0300

    the task09 is done

--- src/ua/javarush/java/core/level03/task09/Solution.java
+++ src/ua/javarush/java/core/level03/task09/Solution.java
@@ -3,9 +3,9 @@ package ua.javarush.java.core.level03.task09;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо змінну типу boolean з іменем isJavaFun
-
+        boolean isJavaFun = true;
 
         // Виводимо значення змінної isJavaFun на екран
-
+        System.out.println(isJavaFun);
     }
 }
