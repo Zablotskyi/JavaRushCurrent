6754e27219f178ab00e6247f695faeb50c88c0bd
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-09 15:48:15 +0300

    started to perform the task09 'Пошук правди'

---
+++ src/ua/javarush/java/core/level03/task09/Solution.java
@@ -0,0 +1,11 @@
+package ua.javarush.java.core.level03.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголошуємо змінну типу boolean з іменем isJavaFun
+
+
+        // Виводимо значення змінної isJavaFun на екран
+
+    }
+}
