72c15868680869a0d480c2d7c7a20cbc6b342d23
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-09 15:55:57 +0300

    the task10 is done

--- src/ua/javarush/java/core/level03/task10/Solution.java
+++ src/ua/javarush/java/core/level03/task10/Solution.java
@@ -3,12 +3,13 @@ package ua.javarush.java.core.level03.task10;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо та присвоюємо рахунок нашої команди й суперника
-
+        int ourScore = 3;
+        int opponentScore = 1;
 
         // weWon — результат логічного порівняння: true, якщо наш рахунок більший
-
+        boolean weWon = ourScore > opponentScore;
 
         // Виводимо значення змінної weWon на екран
-
+        System.out.println(weWon);
     }
 }
