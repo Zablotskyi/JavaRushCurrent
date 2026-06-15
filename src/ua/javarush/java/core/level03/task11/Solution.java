25194362363966255275769b21e64198fac3475b
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-15 16:30:29 +0300

    started to perform the task12 'Комфортна температура в приміщенні'

--- src/ua/javarush/java/core/level03/task11/Solution.java
+++ src/ua/javarush/java/core/level03/task11/Solution.java
@@ -3,16 +3,24 @@ package ua.javarush.java.core.level03.task11;
 public class Solution {
     public static void main(String[] args) {
         // Початкові умови (можна змінювати для перевірки різних варіантів)
-
+        boolean isSunny = true;
+        boolean isWeekend = true;
+        boolean canGoToPark = false;
+        boolean canStayHome = false;
 
         // До парку йдемо лише якщо водночас сонячно Й вихідні
-
+        if (isSunny == true && isWeekend == true) {
+            canGoToPark = true;
+        }
 
         // Залишаємось удома, якщо НЕ сонячно АБО НЕ вихідні
-
+        if (!isSunny || !isWeekend) {
+            canStayHome = true;
+        }
 
         // Виведення значень на екран
-
+        System.out.println(canGoToPark);
+        System.out.println(canStayHome);
 
     }
 }
