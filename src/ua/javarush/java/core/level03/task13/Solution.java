1af60484b24e0bf6aac49d01c1ac834cc31b6728
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-16 11:36:23 +0300

    started to perform the task13 'Пріоритет логічних операторів'

---
+++ src/ua/javarush/java/core/level03/task13/Solution.java
@@ -0,0 +1,15 @@
+package ua.javarush.java.core.level03.task13;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголошуємо початкові булеві змінні
+
+
+        // Важливо: оператор && має вищий пріоритет, ніж ||
+        // Тому спочатку обчиснюється (hasPlaneTickets && hasHotelRooms), потім результат об’єднується з toTheSea через ||
+
+
+        // Виводимо результат на екран
+
+    }
+}
