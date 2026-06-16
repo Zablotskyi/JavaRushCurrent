8e0efc22b41476411c1fc2a902b16efb4874c4b2
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-16 11:38:28 +0300

    the task13 is done

--- src/ua/javarush/java/core/level03/task13/Solution.java
+++ src/ua/javarush/java/core/level03/task13/Solution.java
@@ -3,13 +3,15 @@ package ua.javarush.java.core.level03.task13;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо початкові булеві змінні
-
+        boolean toTheSea = true;
+        boolean hasPlaneTickets = false;
+        boolean hasHotelRooms = true;
 
         // Важливо: оператор && має вищий пріоритет, ніж ||
         // Тому спочатку обчиснюється (hasPlaneTickets && hasHotelRooms), потім результат об’єднується з toTheSea через ||
-
+        boolean vacationHappened = toTheSea || hasPlaneTickets && hasHotelRooms;
 
         // Виводимо результат на екран
-
+        System.out.println(vacationHappened);
     }
 }
