cf08a3dbfe5066c6313ee5ef8e4125d958b07332
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-17 13:44:08 +0300

    the task14 is done

--- src/ua/javarush/java/core/level03/task14/Solution.java
+++ src/ua/javarush/java/core/level03/task14/Solution.java
@@ -3,16 +3,19 @@ package ua.javarush.java.core.level03.task14;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо та ініціалізуємо змінні згідно з умовою задачі
-
+        boolean ticketsOnSale = false;
+        boolean haveMoneyForTicket = true;
+        boolean concertNotCanceled = false;
 
         // Перший логічний вираз без додаткових дужок
-
+        boolean canBuy = ticketsOnSale || haveMoneyForTicket && concertNotCanceled;
 
         // Другий логічний вираз із додаванням дужок
-
+        boolean canBuyWithNewMeaning = (ticketsOnSale || haveMoneyForTicket) && concertNotCanceled;
 
         // Виводимо результати обох виразів на екран
-
+        System.out.println(canBuy);
+        System.out.println(canBuyWithNewMeaning);
 
     }
 }
