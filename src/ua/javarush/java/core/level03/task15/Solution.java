981837eaf7f0854f02555d9d291760114663a5b3
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-17 14:06:59 +0300

    the task15 is done

--- src/ua/javarush/java/core/level03/task15/Solution.java
+++ src/ua/javarush/java/core/level03/task15/Solution.java
@@ -3,19 +3,23 @@ package ua.javarush.java.core.level03.task15;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо та ініціалізуємо змінні для умов допуску
-
+        boolean hasInvitation = true;
+        boolean dressCodeMet = false;
+        boolean passwordIsCorrect = true;
 
         // Основний логічний вираз без додаткових дужок
-
+        boolean admitted = hasInvitation && dressCodeMet && passwordIsCorrect;
 
         // Вираз із групуванням перших двох умов
-
+        boolean first = (hasInvitation && dressCodeMet) && passwordIsCorrect;
 
         // Вираз із групуванням останніх двох умов
-
+        boolean second = hasInvitation && (dressCodeMet && passwordIsCorrect);
 
         // Виводимо результати всіх трьох виразів на екран
-
+        System.out.println(admitted);
+        System.out.println(first);
+        System.out.println(second);
 
     }
 }
