54518e79defb610b257ac8f23a3c78b37e77cbf2
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-18 13:47:01 +0300

    the task20 is done

--- src/ua/javarush/java/core/level03/task20/Solution.java
+++ src/ua/javarush/java/core/level03/task20/Solution.java
@@ -3,12 +3,14 @@ package ua.javarush.java.core.level03.task20;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо змінну віку відвідувача
-
+        int visitorAge = 6;
 
         // Використовуємо вкладені тернарні оператори для визначення категорії квитка
-
+        String ticketCategory = (visitorAge < 7) ? "Дитячий" :
+                (visitorAge < 18) ? "Підлітковий" :
+                        (visitorAge < 65) ? "Дорослий" : "Пенсійний";
 
         // Виводимо категорію квитка на екран
-
+        System.out.println(ticketCategory);
     }
 }
