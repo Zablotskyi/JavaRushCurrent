5ba2a37e4f767f7065be58b835ee862630de3812
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-18 16:31:33 +0300

    started to perform the task01 'Зворотний відлік перед стартом'

---
+++ src/ua/javarush/java/core/level04/task01/Solution.java
@@ -0,0 +1,19 @@
+package ua.javarush.java.core.level04.task01;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Ініціалізуємо змінну для зворотного відліку
+
+
+        // Цикл while виконується, доки значення countdown більше 0
+        while (/* умова */) {
+            // Виводимо поточне значення countdown
+
+            // Зменшуємо значення countdown на 1
+
+        }
+
+        // Після завершення циклу виводимо повідомлення "Старт!"
+
+    }
+}
