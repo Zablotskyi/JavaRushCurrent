9b0281d6198c10f5eda3803d359f8d75e159784f
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-18 11:13:00 +0300

    the task17 is done

--- src/ua/javarush/java/core/level03/task17/Solution.java
+++ src/ua/javarush/java/core/level03/task17/Solution.java
@@ -3,12 +3,13 @@ package ua.javarush.java.core.level03.task17;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо дві змінні для часу спортсменів
-
+        int alexsTime = 10;
+        int mikesTime = 9;
 
         // Використовуємо тернарний оператор для визначення мінімального часу
-
+        int fastestTime = (alexsTime < mikesTime) ? alexsTime : mikesTime;
 
         // Виводимо на екран найменший час
-
+        System.out.println(fastestTime);
     }
 }
