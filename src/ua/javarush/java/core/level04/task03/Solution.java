8a45dd3807eb2d77a3b6c33dd548b183ffa43ee5
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-18 16:56:47 +0300

    the task03 is done

--- src/ua/javarush/java/core/level04/task03/Solution.java
+++ src/ua/javarush/java/core/level04/task03/Solution.java
@@ -3,14 +3,15 @@ package ua.javarush.java.core.level04.task03;
 public class Solution {
     public static void main(String[] args) {
         // Ініціалізуємо змінну для номера крісла
-
+        int place = 2;
 
         // Використовуємо цикл while для перебирання номерів крісел
-        while (/* умова */) {
+        while (place <= 10) {
             // Виводимо поточний номер крісла на новому рядку
+            System.out.println(place);
 
             // Збільшуємо номер крісла на 2
-
+            place += 2;
         }
     }
 }
