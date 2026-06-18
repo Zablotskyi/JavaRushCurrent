f6dba01ce64fced9a12f7cbf707d077629cd5b39
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-18 16:34:31 +0300

    task01 is done

--- src/ua/javarush/java/core/level04/task01/Solution.java
+++ src/ua/javarush/java/core/level04/task01/Solution.java
@@ -3,17 +3,17 @@ package ua.javarush.java.core.level04.task01;
 public class Solution {
     public static void main(String[] args) {
         // Ініціалізуємо змінну для зворотного відліку
-
+        int count = 3;
 
         // Цикл while виконується, доки значення countdown більше 0
-        while (/* умова */) {
+        while (count > 0) {
             // Виводимо поточне значення countdown
-
+            System.out.println(count);
             // Зменшуємо значення countdown на 1
-
+            count--;
         }
 
         // Після завершення циклу виводимо повідомлення "Старт!"
-
+        System.out.println("Старт!");
     }
 }
