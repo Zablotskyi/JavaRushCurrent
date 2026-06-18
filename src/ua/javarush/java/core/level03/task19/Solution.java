ea758fc7a11579acb7440479ddab6253da7fe1f2
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-18 13:18:43 +0300

    the task19 is done

--- src/ua/javarush/java/core/level03/task19/Solution.java
+++ src/ua/javarush/java/core/level03/task19/Solution.java
@@ -3,12 +3,12 @@ package ua.javarush.java.core.level03.task19;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо змінну currentHour, яка зберігає поточну годину (від 0 до 23)
-
+        int currentHour = 23;
 
         // Використовуємо тернарний оператор для вибору привітання
-
+        String greetingMessage = (currentHour < 12) ? "Добрий ранок" : "Добрий день";
 
         // Виводимо привітальне повідомлення на екран
-
+        System.out.println(greetingMessage);
     }
 }
