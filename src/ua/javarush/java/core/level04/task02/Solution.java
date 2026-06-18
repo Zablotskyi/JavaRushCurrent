783f1eb8a56e11db04b5ee877b42e44ad3c0136e
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-18 16:42:41 +0300

    started to perform the task02 'Розблокування смартфона'

---
+++ src/ua/javarush/java/core/level04/task02/Solution.java
@@ -0,0 +1,19 @@
+package ua.javarush.java.core.level04.task02;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        String input = ""; // зберігатимемо введений користувачем текст
+
+        // Запитуємо пароль, доки користувач не введе точне слово "java"
+        while (/* умова */) {
+
+        }
+
+        // Повідомлення після успішного введення
+
+    }
+}
