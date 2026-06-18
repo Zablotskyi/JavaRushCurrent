253a7fa2b88f1dbec689f9d1e2f101f1c841565b
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-18 13:46:16 +0300

    started to perform the task20 'Категорія квитка за віком'

---
+++ src/ua/javarush/java/core/level03/task20/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.java.core.level03.task20;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголошуємо змінну віку відвідувача
+
+
+        // Використовуємо вкладені тернарні оператори для визначення категорії квитка
+
+
+        // Виводимо категорію квитка на екран
+
+    }
+}
