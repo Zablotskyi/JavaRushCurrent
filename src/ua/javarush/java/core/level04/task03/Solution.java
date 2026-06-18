48ee93052a5c7ea6f98590736727bca88f4e93f3
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-18 16:53:48 +0300

    started to perform the task03 'Виведення місць у кінотеатрі'

---
+++ src/ua/javarush/java/core/level04/task03/Solution.java
@@ -0,0 +1,16 @@
+package ua.javarush.java.core.level04.task03;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Ініціалізуємо змінну для номера крісла
+
+
+        // Використовуємо цикл while для перебирання номерів крісел
+        while (/* умова */) {
+            // Виводимо поточний номер крісла на новому рядку
+
+            // Збільшуємо номер крісла на 2
+
+        }
+    }
+}
