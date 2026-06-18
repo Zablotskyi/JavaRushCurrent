8f629436c1bbcd3fb2c575eecc16cb29ef7e8ea6
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-18 11:15:00 +0300

    started to perform the tax18 'Перевірка номера замовлення на парність'

---
+++ src/ua/javarush/java/core/level03/task18/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.java.core.level03.task18;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголоси змінну orderNumber і присвой їй значення
+
+
+        // Використай тернарний оператор для визначення парності orderNumber
+
+
+        // Виведи результат на екран
+
+    }
+}
