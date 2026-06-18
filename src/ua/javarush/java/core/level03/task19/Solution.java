91b74d1512aac7560e89a6c4c87baa6d49932fdf
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-18 13:15:47 +0300

    started to perform the task19 'Привітання користувача в застосунку'

---
+++ src/ua/javarush/java/core/level03/task19/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.java.core.level03.task19;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголошуємо змінну currentHour, яка зберігає поточну годину (від 0 до 23)
+
+
+        // Використовуємо тернарний оператор для вибору привітання
+
+
+        // Виводимо привітальне повідомлення на екран
+
+    }
+}
