48f5c4462196f7951292bbb65655ee280b16913e
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-18 11:18:29 +0300

    the task18 is done

--- src/ua/javarush/java/core/level03/task18/Solution.java
+++ src/ua/javarush/java/core/level03/task18/Solution.java
@@ -3,12 +3,12 @@ package ua.javarush.java.core.level03.task18;
 public class Solution {
     public static void main(String[] args) {
         // Оголоси змінну orderNumber і присвой їй значення
-
+        int orderNumber = 69;
 
         // Використай тернарний оператор для визначення парності orderNumber
-
+        String orderStatus = (orderNumber % 2 == 0) ? "Парний" : "Непарний";
 
         // Виведи результат на екран
-
+        System.out.println(orderStatus);
     }
 }
