1b9022ec0b2af854af9f0220d6b5b238197fcd7b
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-18 11:09:08 +0300

    started to perform the task17 'Вибір переможця в змаганні'

---
+++ src/ua/javarush/java/core/level03/task17/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.java.core.level03.task17;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголошуємо дві змінні для часу спортсменів
+
+
+        // Використовуємо тернарний оператор для визначення мінімального часу
+
+
+        // Виводимо на екран найменший час
+
+    }
+}
