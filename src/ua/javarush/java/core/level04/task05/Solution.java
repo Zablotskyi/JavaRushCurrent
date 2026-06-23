9934896dd5706c2ff50386d2086c5f602279ea45
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-06-23 22:02:45 +0300

    started to perform the task05 'Зворотний відлік до Нового року'

---
+++ src/ua/javarush/java/core/level04/task05/Solution.java
@@ -0,0 +1,13 @@
+package ua.javarush.java.core.level04.task05;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Цикл for для зворотного відліку від 5 до 1
+        for (/* ініціалізація */; /* умова */; /* зміна */) {
+            // Виведення поточного числа з нового рядка
+
+        }
+        // Виведення привітання після завершення циклу
+
+    }
+}
