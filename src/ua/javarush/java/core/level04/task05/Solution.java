470bb239b3a3a733ef4050e9c701ce37ad0b9912
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-07-13 16:17:43 +0300

    the task05 is done

--- src/ua/javarush/java/core/level04/task05/Solution.java
+++ src/ua/javarush/java/core/level04/task05/Solution.java
@@ -3,11 +3,11 @@ package ua.javarush.java.core.level04.task05;
 public class Solution {
     public static void main(String[] args) {
         // Цикл for для зворотного відліку від 5 до 1
-        for (/* ініціалізація */; /* умова */; /* зміна */) {
+        for (int i = 5/* ініціалізація */; i > 0 /* умова */; i-- /* зміна */) {
             // Виведення поточного числа з нового рядка
-
+            System.out.println(i);
         }
         // Виведення привітання після завершення циклу
-
+        System.out.println("З Новим роком!");
     }
 }
