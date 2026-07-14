79972f4d9b13ee86af57afa94e4ba1836dc4372b
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-07-14 15:41:16 +0300

    the task12 is done

--- src/ua/javarush/java/core/level04/task12/Solution.java
+++ src/ua/javarush/java/core/level04/task12/Solution.java
@@ -5,20 +5,21 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Створюємо Scanner для зчитування пароля з клавіатури
-
+        Scanner scanner = new Scanner(System.in);
 
         // змінна для введеного пароля
-
+        String password;
 
         // Цикл do-while гарантує щонайменше один запит пароля
         do {
             // Запитуємо введення пароля
-
+            System.out.println("Введіть надійний пароль:");
             // зчитуємо рядок повністю
+            password = scanner.nextLine();
 
-        } while (/* умова */); // повторюємо, доки пароль коротший за 6 символів
+        } while (password.length() < 6/* умова */); // повторюємо, доки пароль коротший за 6 символів
 
         // Повідомлення про успішну реєстрацію
-
+        System.out.println("Пароль надійний, акаунт створено!");
     }
 }
