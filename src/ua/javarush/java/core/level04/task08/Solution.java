a3e2af6275edfaeccf65c512a25a17f84a80c21c
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-07-14 11:14:04 +0300

    started to perform the task08 'Координати рухомих об’єктів'

---
+++ src/ua/javarush/java/core/level04/task08/Solution.java
@@ -0,0 +1,11 @@
+package ua.javarush.java.core.level04.task08;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Використовуємо цикл for для одночасної зміни двох змінних
+        for (/* ініціалізація */; /* умова */; /* зміна */) {
+            // На кожній ітерації виводимо поточні значення x і y через пробіл
+
+        }
+    }
+}
