d7c5be4982a980a446d46d224a6675c659393ca9
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-07-14 11:54:34 +0300

    started to perform the task09 'Перше завантаження гри'

---
+++ src/ua/javarush/java/core/level04/task09/Solution.java
@@ -0,0 +1,16 @@
+package ua.javarush.java.core.level04.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголошуємо змінну gameVersion і присвоюємо їй значення 10
+
+
+        // Використовуємо цикл do-while для виконання дій принаймні один раз
+        do {
+            // Виводимо повідомлення "Завантаження даних..."
+
+            // Виводимо значення змінної gameVersion
+
+        } while (/* умова */); // Умова виходу з циклу відразу хибна
+    }
+}
