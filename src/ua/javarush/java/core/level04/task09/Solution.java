82407765bf051b26df6fcd1a538ba4dca968cc4c
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-07-14 13:55:35 +0300

    the task09 is done

--- src/ua/javarush/java/core/level04/task09/Solution.java
+++ src/ua/javarush/java/core/level04/task09/Solution.java
@@ -3,14 +3,14 @@ package ua.javarush.java.core.level04.task09;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо змінну gameVersion і присвоюємо їй значення 10
-
+        int gameVersion = 10;
 
         // Використовуємо цикл do-while для виконання дій принаймні один раз
         do {
             // Виводимо повідомлення "Завантаження даних..."
-
+            System.out.println("Завантаження даних...");
             // Виводимо значення змінної gameVersion
-
-        } while (/* умова */); // Умова виходу з циклу відразу хибна
+            System.out.println(gameVersion);
+        } while (false/* умова */); // Умова виходу з циклу відразу хибна
     }
 }
