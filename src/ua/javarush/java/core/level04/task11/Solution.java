783489204bdcfbd64aedf675be5001c1dca00ec4
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-07-14 14:54:45 +0300

    the task11 is done

--- src/ua/javarush/java/core/level04/task11/Solution.java
+++ src/ua/javarush/java/core/level04/task11/Solution.java
@@ -5,24 +5,27 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Створюємо Scanner для зчитування введення користувача
-
+        Scanner scanner = new Scanner(System.in);
 
         // змінна для зберігання обраного пункту меню
-
+        int item;
 
         // Використовуємо do-while, щоб меню показувалося щонайменше один раз і повторювалося до вибору "Вийти"
         do {
             // Виводимо меню в кожному циклі
+            System.out.println("Меню:");
+            System.out.println("1. Вивести привітання");
+            System.out.println("2. Вийти");
 
             // Зчитуємо номер обраного пункту
-
+            item = scanner.nextInt();
 
             // Обробка вибору
-            if (/* умова */) {
-
+            if (item == 1/* умова */) {
+                System.out.println("Привіт!");
             }
             // За вибору 2 цикл завершиться завдяки умові while нижче
             // Для інших значень просто знову покажемо меню
-        } while (/* умова */);
+        } while (item != 2/* умова */);
     }
 }
