168df062c9874507ec436978d377878f3ca0f541
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-07-14 14:25:34 +0300

    started to perform the task10 'Введення PIN-коду'

--- src/ua/javarush/java/core/level04/task10/Solution.java
+++ src/ua/javarush/java/core/level04/task10/Solution.java
@@ -5,20 +5,18 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Створюємо Scanner для читання даних із клавіатури
-        Scanner scanner = new Scanner(System.in);
-        int PIN;
+
 
         // Використовуємо do-while, щоб запитати PIN хоча б один раз
         // і повторювати введення, поки число не буде в діапазоні 1000..9999
         do {
             // Запит введення PIN-коду
-            System.out.println("Введіть PIN код");
-            // читаємо ціле число
-            PIN = scanner.nextInt();
 
-        } while (PIN < 1000 || PIN > 9999 /* умова */);
+            // читаємо ціле число
+
+        } while (/* умова */);
 
         // Виводимо повідомлення про прийняття PIN-коду
-        System.out.println("PIN-код прийнято");
+
     }
 }
