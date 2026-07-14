3ded56109e16be5cd1d9537773442a7394ccc072
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-07-14 11:16:53 +0300

    the task08 is done

--- src/ua/javarush/java/core/level04/task08/Solution.java
+++ src/ua/javarush/java/core/level04/task08/Solution.java
@@ -3,9 +3,9 @@ package ua.javarush.java.core.level04.task08;
 public class Solution {
     public static void main(String[] args) {
         // Використовуємо цикл for для одночасної зміни двох змінних
-        for (/* ініціалізація */; /* умова */; /* зміна */) {
+        for (int x = 1, y = 5/* ініціалізація */; x <= 5 /* умова */; x++, y-- /* зміна */) {
             // На кожній ітерації виводимо поточні значення x і y через пробіл
-
+            System.out.println(x + " " + y);
         }
     }
 }
