0e2a4c6f558055f04ddcd6da98d9e66c9833f9b7
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-07-28 16:19:54 +0300

    the task18 is done

--- src/ua/javarush/java/core/level04/task18/Solution.java
+++ src/ua/javarush/java/core/level04/task18/Solution.java
@@ -3,14 +3,15 @@ package ua.javarush.java.core.level04.task18;
 public class Solution {
     public static void main(String[] args) {
         // Зовнішній цикл відповідає за рядки (5 рядків)
-        for (/* ініціалізація */; /* умова */; /* зміна */) {
+        for (int i = 0; i < 5; i++) {
             // Внутрішній цикл відповідає за стовпці (7 стовпців)
-            for (/* ініціалізація */; /* умова */; /* зміна */) {
+            for (int j = 0; j < 7; j++) {
                 // Виводимо символ "⭐" із пробілом
-
+                System.out.print("⭐");
+                System.out.print(" ");
             }
             // Після завершення рядка переходимо на новий рядок
-
+            System.out.println();
         }
     }
 }
