e6f117e9f938202cdbbccb2b08ccda393f4cafd7
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-04 12:54:33 +0300

    started to perform the task04 'Магічний гримуар зростання сили'

---
+++ src/ua/javarush/java/core/level05/task04/Solution.java
@@ -0,0 +1,19 @@
+package ua.javarush.java.core.level05.task04;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Створюємо обʼєкт Scanner для введення даних з клавіатури
+
+
+        // Просимо користувача ввести число N (magicLimit)
+
+
+        // Обчислюємо та виводимо квадрат кожного числа від 1 до N
+        for (/* ініціалізація */; /* умова */; /* зміна */) {
+            // Виведення квадрата числа
+
+        }
+    }
+}
