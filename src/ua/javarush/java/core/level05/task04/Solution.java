d93d403e5ce9cc64880b182dd32bdca4aee76f7c
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-04 13:58:29 +0300

    the task04 is done

--- src/ua/javarush/java/core/level05/task04/Solution.java
+++ src/ua/javarush/java/core/level05/task04/Solution.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Створюємо обʼєкт Scanner для введення даних з клавіатури
-
+        Scanner scanner = new Scanner(System.in);
 
         // Просимо користувача ввести число N (magicLimit)
-
+        System.out.println("Введіть верхню межу чисел: ");
+        int magicLimit = scanner.nextInt();
 
         // Обчислюємо та виводимо квадрат кожного числа від 1 до N
-        for (/* ініціалізація */; /* умова */; /* зміна */) {
+        for (int i = 1; i <= magicLimit; i++) {
             // Виведення квадрата числа
-
+            System.out.println(i * i);
         }
     }
 }
