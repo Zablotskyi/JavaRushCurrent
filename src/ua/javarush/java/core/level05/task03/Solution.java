ad2ac9548a22b26af6a5b257d2d3e63ef9e35880
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-04 12:16:11 +0300

    the task03 is done

--- src/ua/javarush/java/core/level05/task03/Solution.java
+++ src/ua/javarush/java/core/level05/task03/Solution.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
 
         // Попроси користувача ввести рядок (послання)
-
+        System.out.println("Введіть рядок (послання): ");
+        String magicMessage = scanner.nextLine();
+        String newLine = "";
 
         // Розгорни рядок за допомогою циклу for
-
+        for (int i = magicMessage.length() - 1; i >= 0; i--) {
+            newLine = newLine + magicMessage.charAt(i);
+        }
 
         // Виведи інвертований рядок
+        System.out.println(newLine);
 
     }
 }
