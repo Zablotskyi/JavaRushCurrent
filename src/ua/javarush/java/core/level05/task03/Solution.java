7ca5c01e48db3d57181f05ae63219fc00f0b9f1a
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-04 12:04:15 +0300

    started to perform the task 'Дзеркальне послання чарівника'

---
+++ src/ua/javarush/java/core/level05/task03/Solution.java
@@ -0,0 +1,18 @@
+package ua.javarush.java.core.level05.task03;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+
+        // Попроси користувача ввести рядок (послання)
+
+
+        // Розгорни рядок за допомогою циклу for
+
+
+        // Виведи інвертований рядок
+
+    }
+}
