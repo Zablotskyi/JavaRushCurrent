9c6337af0f84e46e3884783509d61b95eb4ee091
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-05 16:35:23 +0300

    started to perform the task07 'Підрахунок скарбів у чарівній скрині'

---
+++ src/ua/javarush/java/core/level05/task07/Solution.java
@@ -0,0 +1,17 @@
+package ua.javarush.java.core.level05.task07;
+
+import java.util.Scanner;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        // Створюємо об’єкт Scanner для зчитування даних з консолі
+
+
+        // Зчитуємо велике число, введене користувачем
+
+
+        // Виводимо кількість цифр у числі
+
+    }
+}
