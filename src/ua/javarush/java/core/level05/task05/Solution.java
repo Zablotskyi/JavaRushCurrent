e24806cee6c72f427e7c99f51d7068a678d43bf1
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-05 11:40:09 +0300

    the task05 is done

--- src/ua/javarush/java/core/level05/task05/Solution.java
+++ src/ua/javarush/java/core/level05/task05/Solution.java
@@ -5,26 +5,28 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Створюємо сканер для зчитування числа з консолі
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть висоту обеліска: ");
+        int pyramidHeight = scanner.nextInt();
 
         // Будуємо обеліск рядок за рядком
-        for (/* ініціалізація */; /* умова */; /* зміна */) {
+        for (int i = 1; i <= pyramidHeight; i++) {
             // Кількість пробілів ліворуч: зменшується від (N - 1) до 0
-
 
             // Кількість зірочок: зростає за формулою (2 * row - 1)
 
-
             // Друкуємо пробіли для вирівнювання
-            for (/* ініціалізація */; /* умова */; /* зміна */) {
-
+            for (int j = pyramidHeight; j > i; j--) {
+                System.out.print(" ");
             }
+
             // Друкуємо зірочки
-            for (/* ініціалізація */; /* умова */; /* зміна */) {
-
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
             }
-            // Перехід на новий рядок після кожного рядка обеліска
 
+            // Перехід на новий рядок після кожного рядка обеліска
+            System.out.println();
         }
     }
 }
