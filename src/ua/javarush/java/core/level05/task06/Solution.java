612e6ebb22af09c49d87f639cad781bdd2036783
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-05 15:29:12 +0300

    the task06 is done

--- src/ua/javarush/java/core/level05/task06/Solution.java
+++ src/ua/javarush/java/core/level05/task06/Solution.java
@@ -5,20 +5,27 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Створюємо Scanner для читання рядка з клавіатури
-
+        Scanner scanner = new Scanner(System.in);
+        String ancientRiddle = scanner.nextLine();
+        String tempAncientRiddle = "";
 
         // Перевіряємо паліндром двома вказівниками: з початку і з кінця
+        boolean flag = true;
 
-
-        for (/* ініціалізація */; /* умова */; /* зміна */) {
+        for (int i = ancientRiddle.length() - 1, j = 0; i >= 0; i--, j++) {
             // Перевірка чутлива до регістру, порівнюємо символи напряму
-            if (/* умова */) {
-
+            if (ancientRiddle.charAt(i) != ancientRiddle.charAt(j)) {
+                flag = false;
                 break; // Далі перевіряти не потрібно
             }
+
         }
 
         // Вивід суворо "YES" або "NO" без додаткових повідомлень
-
+        if (flag) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
