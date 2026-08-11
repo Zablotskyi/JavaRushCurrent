f6c9324283c4963737e2509ec77ae928aa563169
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-11 10:12:48 +0300

    the task08 is done

--- src/ua/javarush/java/core/level05/task08/Solution.java
+++ src/ua/javarush/java/core/level05/task08/Solution.java
@@ -5,15 +5,23 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Створюємо Scanner для читання даних з клавіатури
-
+        Scanner scanner = new Scanner(System.in);
 
         // Зчитуємо три цілих числа — тривалості трьох шляхів у хвилинах
-
+        int pathOne = scanner.nextInt();
+        int pathTwo = scanner.nextInt();
+        int pathThree = scanner.nextInt();
 
         // Визначаємо мінімальну тривалість серед трьох значень
-
+        int minPath;
+        if (pathOne <= pathTwo && pathOne <= pathThree)
+            minPath = pathOne;
+        else if (pathTwo <= pathOne && pathTwo <= pathThree)
+            minPath = pathTwo;
+        else
+            minPath = pathThree;
 
         // Виводимо результат
-
+        System.out.println(minPath);
     }
 }
