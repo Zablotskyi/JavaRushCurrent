e2de511c75105d43e1d8579c8268d95260dda53d
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-11 10:44:36 +0300

    started to perfrom the task11 'Гра «Вгадай число»'

---
+++ src/ua/javarush/java/core/level05/task11/Solution.java
@@ -0,0 +1,17 @@
+package ua.javarush.java.core.level05.task11;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int secret = rand.nextInt(100) + 1; // 1..100
+        Scanner console = new Scanner(System.in);
+
+        // Гра з духом
+        while (true) {
+
+        }
+    }
+}
