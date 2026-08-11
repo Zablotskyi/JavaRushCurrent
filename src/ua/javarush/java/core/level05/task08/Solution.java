d32ddb8827a65b5978cde981092fac3247c22542
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-11 10:02:55 +0300

    started to perform the task08 'Перегони за золотим кубком: найкоротший шлях'

---
+++ src/ua/javarush/java/core/level05/task08/Solution.java
@@ -0,0 +1,19 @@
+package ua.javarush.java.core.level05.task08;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Створюємо Scanner для читання даних з клавіатури
+
+
+        // Зчитуємо три цілих числа — тривалості трьох шляхів у хвилинах
+
+
+        // Визначаємо мінімальну тривалість серед трьох значень
+
+
+        // Виводимо результат
+
+    }
+}
