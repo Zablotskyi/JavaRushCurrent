ccd45cdc1211e129cb4a6297eb274b6287e38932
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-11 09:42:31 +0300

    the task07 is done

--- src/ua/javarush/java/core/level05/task07/Solution.java
+++ src/ua/javarush/java/core/level05/task07/Solution.java
@@ -1,17 +1,27 @@
 package ua.javarush.java.core.level05.task07;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
+
+import static java.math.BigDecimal.*;
 
 public class Solution {
     
     public static void main(String[] args) {
         // Створюємо об’єкт Scanner для зчитування даних з консолі
-
+        Scanner scanner = new Scanner(System.in);
 
         // Зчитуємо велике число, введене користувачем
+        String treasureAmount = scanner.nextLine();
 
+        treasureAmount.trim();
+
+        if (treasureAmount.startsWith("-")) {
+            treasureAmount = treasureAmount.substring(1);
+        }
 
         // Виводимо кількість цифр у числі
-
+        System.out.println(treasureAmount.length());
     }
 }
