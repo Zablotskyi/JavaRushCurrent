2db281adfb90a5f65ea85199f869fa770b74f5f2
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-26 16:05:17 +0300

    started to perform the task16 'Облік двох календарних систем: Час із різних вимірів'

---
+++ src/ua/javarush/java/core/level05/task16/Main.java
@@ -0,0 +1,17 @@
+package ua.javarush.java.core.level05.task16;
+
+// Імпортуємо клас java.util.Date
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        // Створюємо об’єкт java.util.Date і присвоюємо йому поточні дату і час
+
+
+        // Створюємо об’єкт java.sql.Date, використовуючи повну назву класу, і присвоюємо йому поточну дату
+
+
+        // Виводимо обидва об’єкти у консоль
+
+    }
+}
