07b60a76b46778d1f6793648a40fce2183054ba0
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-26 16:03:41 +0300

    the task15 is done

--- src/ua/javarush/java/core/level05/task15/Main.java
+++ src/ua/javarush/java/core/level05/task15/Main.java
@@ -3,12 +3,12 @@ package ua.javarush.java.core.level05.task15;
 public class Main {
     public static void main(String[] args) {
         // Створюємо об’єкт Scanner, використовуючи повне ім’я класу
-
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
 
         // Зчитуємо рядок, введений користувачем
-
+        String line = scanner.nextLine();
 
         // Виводимо зчитаний рядок на екран
-        
+        System.out.println(line);
     }
 }
