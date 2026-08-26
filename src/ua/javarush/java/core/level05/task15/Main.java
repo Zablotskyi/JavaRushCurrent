b0ef7bac9c36e3eb57fc5da482f37fe719de7bd5
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-26 16:00:11 +0300

    started to perform the task 'Використання інструмента за повною адресою'

---
+++ src/ua/javarush/java/core/level05/task15/Main.java
@@ -0,0 +1,14 @@
+package ua.javarush.java.core.level05.task15;
+
+public class Main {
+    public static void main(String[] args) {
+        // Створюємо об’єкт Scanner, використовуючи повне ім’я класу
+
+
+        // Зчитуємо рядок, введений користувачем
+
+
+        // Виводимо зчитаний рядок на екран
+        
+    }
+}
