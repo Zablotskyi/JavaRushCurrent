74356f88e1cd9aa235ad91c1f92f74d67d828eed
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-31 14:29:20 +0300

    the task20 is done

--- src/ua/javarush/java/core/level05/task20/Solution.java
+++ src/ua/javarush/java/core/level05/task20/Solution.java
@@ -3,14 +3,14 @@ package ua.javarush.java.core.level05.task20;
 public class Solution {
     public static void main(String[] args) {
         // Зовнішній цикл для чисел від 1 до 10 (рядки таблиці)
-
+        for (int i = 1; i <= 10; i++) {
             // Внутрішній цикл для чисел від 1 до 10 (стовпці таблиці)
-
+            for (int j = 1; j <= 10; j++) {
                 // Виведення добутку з роздільником табуляції
-
-
+                System.out.printf("%3d", i * j);
+            }
             // Перехід на новий рядок після завершення рядка таблиці
-
-
+            System.out.println();
+        }
     }
 }
