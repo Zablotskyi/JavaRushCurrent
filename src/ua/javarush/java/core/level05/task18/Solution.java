311a134b7daa36a2f93d2d86535fb48c12681d11
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-31 14:05:03 +0300

    the task18 is done

--- src/ua/javarush/java/core/level05/task18/Solution.java
+++ src/ua/javarush/java/core/level05/task18/Solution.java
@@ -3,19 +3,19 @@ package ua.javarush.java.core.level05.task18;
 public class Solution {
     public static void main(String[] args) {
         // Починаємо з числа 1
-
+        int number = 1;
 
         // Використовуємо цикл while для перебору чисел
         while (true) {
             // Перевіряємо, чи ділиться поточне число на 7 без остачі
-            if (/* умова */) {
+            if (number % 7 == 0) {
                 // Якщо ділиться, виводимо результат
-
+                System.out.println("Перше число, кратне 7: " + number);
                 // Завершуємо цикл за допомогою break
-
+                break;
             }
             // Збільшуємо число на 1
-
+            number++;
         }
     }
 }
