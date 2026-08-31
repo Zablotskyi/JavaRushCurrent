829812530b6aa4e517d0817bdfafe6b474300f56
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-08-31 14:00:46 +0300

    the task17 is done

--- src/ua/javarush/java/core/level05/task17/Solution.java
+++ src/ua/javarush/java/core/level05/task17/Solution.java
@@ -3,13 +3,13 @@ package ua.javarush.java.core.level05.task17;
 public class Solution {
     public static void main(String[] args) {
         // Перебираємо числа від 1 до 20
-        for (/* ініціалізація */; /* умова */; /* зміна */) {
+        for (int i = 1; i <= 20; i++) {
             // Якщо число ділиться на 3 без остачі, пропускаємо його
-            if (/* умова */) {
-
+            if (i % 3 == 0) {
+                continue;
             }
             // Виводимо число, не переходячи на новий рядок
-
+            System.out.print(i + " ");
         }
     }
 }
