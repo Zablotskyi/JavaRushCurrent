e08d3d874c5922fa155018188581b33698c2d479
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-01 10:17:17 +0300

    the task01 is done

--- src/ua/javarush/java/core/level06/task01/Solution.java
+++ src/ua/javarush/java/core/level06/task01/Solution.java
@@ -3,23 +3,24 @@ package ua.javarush.java.core.level06.task01;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо рівень персонажа (тип byte)
-
+        byte characterLevel = 1;
         // Оголошуємо показник броні (тип short)
-
+        short armorRating = 3;
         // Оголошуємо накопичений досвід (тип int)
-
+        int experiencePoints = 100;
         // Оголошуємо кількість золотих монет (тип long)
-
+        long goldCoins = 1000;
         // Оголошуємо опір магії (тип float)
-
+        float magicResistance = 80f;
         // Оголошуємо шанс критичного удару (тип double)
-
+        double criticalHitChance = 15.3;
 
         // Виводимо кожну характеристику героя з нового рядка
-
-
-
-
-
+        System.out.println(characterLevel);
+        System.out.println(armorRating);
+        System.out.println(experiencePoints);
+        System.out.println(goldCoins);
+        System.out.println(magicResistance);
+        System.out.println(criticalHitChance);
     }
 }
