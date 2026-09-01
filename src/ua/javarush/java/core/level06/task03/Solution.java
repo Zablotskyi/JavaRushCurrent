18af27b3046956ae572649bc6186f6498e00ec8e
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-01 12:11:35 +0300

    started to perform the task03 'Розшифрування давніх рун'

---
+++ src/ua/javarush/java/core/level06/task03/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.java.core.level06.task03;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголоси змінну типу char для зберігання загадкового символу руни
+
+
+        // Перетвори символ на його числовий код (Unicode) і збережи у змінну decryptedCode
+
+
+        // Виведи результат дослідження: рядок і розшифрований числовий код символу
+
+    }
+}
