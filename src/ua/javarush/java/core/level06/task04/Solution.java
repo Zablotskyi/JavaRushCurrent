4d7334406e0c9e5a4f705f5d72fa8c14ff24f902
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-01 12:46:07 +0300

    started to perform the task04 'Двостороння магія символів і чисел'

---
+++ src/ua/javarush/java/core/level06/task04/Solution.java
@@ -0,0 +1,23 @@
+package ua.javarush.java.core.level06.task04;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголошуємо змінну secretSpellCode типу int і надаємо їй значення 8364
+
+
+        // Явно приводимо число secretSpellCode до типу char і зберігаємо в revealedSymbol
+
+
+        // Виводимо символ, що відповідає коду 8364
+
+
+        // Оголошуємо змінну enigmaticLetter типу char і надаємо їй символ 'Ж'
+
+
+        // Явно приводимо символ enigmaticLetter до типу int і зберігаємо в numericEssenceOfLetter
+
+
+        // Виводимо числовий код символу 'Ж'
+
+    }
+}
