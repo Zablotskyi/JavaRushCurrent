76bd1c53f625fe4f60907dba992ec79b946d6fd0
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-01 11:57:53 +0300

    the task02 is done

--- src/ua/javarush/java/core/level06/task02/Solution.java
+++ src/ua/javarush/java/core/level06/task02/Solution.java
@@ -4,9 +4,9 @@ public class Solution {
     public static void main(String[] args) {
         // Оголошуємо змінну dragonHoard типу long і присвоюємо їй 10 мільярдів
         // Використовуємо суфікс L і підкреслення для зручності читання
-
+        long dragonHoard = 10_000_000_000L;
         
         // Виводимо значення змінної на екран
-
+        System.out.println(dragonHoard);
     }
 }
