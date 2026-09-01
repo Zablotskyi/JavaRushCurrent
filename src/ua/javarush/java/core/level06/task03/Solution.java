87995ad3ec0a2f21aa8e887ed80848f0280ceceb
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-01 12:14:14 +0300

    the task03 is done

--- src/ua/javarush/java/core/level06/task03/Solution.java
+++ src/ua/javarush/java/core/level06/task03/Solution.java
@@ -3,12 +3,12 @@ package ua.javarush.java.core.level06.task03;
 public class Solution {
     public static void main(String[] args) {
         // Оголоси змінну типу char для зберігання загадкового символу руни
-
+        char mysteryCharacter = 'Z';
 
         // Перетвори символ на його числовий код (Unicode) і збережи у змінну decryptedCode
-
+        int decryptedCode = mysteryCharacter;
 
         // Виведи результат дослідження: рядок і розшифрований числовий код символу
-
+        System.out.println("Код символу 'Z': " + decryptedCode);
     }
 }
