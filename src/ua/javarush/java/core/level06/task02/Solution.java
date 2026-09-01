cd3c5349379022b666ca0d8c651cc8275d6a7b2f
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-01 11:55:06 +0300

    started to perform the task02 'Підрахунок скарбів у драконовому лігві'

---
+++ src/ua/javarush/java/core/level06/task02/Solution.java
@@ -0,0 +1,12 @@
+package ua.javarush.java.core.level06.task02;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголошуємо змінну dragonHoard типу long і присвоюємо їй 10 мільярдів
+        // Використовуємо суфікс L і підкреслення для зручності читання
+
+        
+        // Виводимо значення змінної на екран
+
+    }
+}
