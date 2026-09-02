9e2bd8b9f51bc2a1ab6618b3f6389c65a24ca5a7
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-02 14:33:42 +0300

    the task04 is done

--- src/ua/javarush/java/core/level06/task04/Solution.java
+++ src/ua/javarush/java/core/level06/task04/Solution.java
@@ -3,21 +3,21 @@ package ua.javarush.java.core.level06.task04;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо змінну secretSpellCode типу int і надаємо їй значення 8364
-
+        int secretSpellCode = 8364;
 
         // Явно приводимо число secretSpellCode до типу char і зберігаємо в revealedSymbol
-
+        char revealedSymbol = (char) secretSpellCode;
 
         // Виводимо символ, що відповідає коду 8364
-
+        System.out.println("Символ з кодом 8364: " + revealedSymbol);
 
         // Оголошуємо змінну enigmaticLetter типу char і надаємо їй символ 'Ж'
-
+        char enigmaticLetter = 'Ж';
 
         // Явно приводимо символ enigmaticLetter до типу int і зберігаємо в numericEssenceOfLetter
-
+        int numericEssenceOfLetter = enigmaticLetter;
 
         // Виводимо числовий код символу 'Ж'
-
+        System.out.println("Код символу 'Ж': " + numericEssenceOfLetter);
     }
 }
