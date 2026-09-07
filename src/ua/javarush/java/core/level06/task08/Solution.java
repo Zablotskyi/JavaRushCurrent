c2008138ba0d506895186869b92000abb8c82a4e
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-07 16:37:32 +0300

    the task08 is done

--- src/ua/javarush/java/core/level06/task08/Solution.java
+++ src/ua/javarush/java/core/level06/task08/Solution.java
@@ -3,15 +3,15 @@ package ua.javarush.java.core.level06.task08;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо загальну кількість магічних яблук
-
+        int totalApples = 7;
 
         // Оголошуємо кількість друзів-авантюристів
-
+        int numberOfFriends = 2;
 
         // Приводимо totalApples до типу double перед діленням, щоб зберегти дробову частину результату
-
+        double applesPerFriend = (double) totalApples / numberOfFriends;
 
         // Виводимо на екран точну частку яблук на одного друга (з дробовою частиною)
-
+        System.out.println(applesPerFriend);
     }
 }
