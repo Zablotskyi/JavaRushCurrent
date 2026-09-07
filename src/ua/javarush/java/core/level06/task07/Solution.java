7e5bfd203b24f5353e068db0f1e4482ebb593ae7
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-07 16:22:45 +0300

    started to perform the task07 'Форматування цін на вітрині онлайн-магазину'

---
+++ src/ua/javarush/java/core/level06/task07/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.java.core.level06.task07;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголошуємо змінну й присвоюємо їй ціну товару
+
+
+        // Форматуємо число так, щоб було рівно два знаки після коми
+
+
+        // Виводимо відформатоване значення на екран
+
+    }
+}
