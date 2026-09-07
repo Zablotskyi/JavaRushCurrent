4331621e2534fd3986a35fe084f156cf44edbf1e
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-07 16:21:55 +0300

    the task06 is done

--- src/ua/javarush/java/core/level06/task06/Solution.java
+++ src/ua/javarush/java/core/level06/task06/Solution.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Створюємо об’єкт Scanner для зчитування даних із клавіатури
-
+        Scanner scanner = new Scanner(System.in);
 
         // Просимо користувача ввести суму платежу
-
+        System.out.println("Введіть суму платежу: ");
 
         // Зчитуємо введене значення та зберігаємо його у змінній типу double
-
+        double customerPayment = scanner.nextDouble();
 
         // Підтверджуємо отриману суму, виводячи її на екран
-
+        System.out.println("Внесено коштів: " + customerPayment);
     }
 }
