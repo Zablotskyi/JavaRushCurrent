df40c55be571b985a1d507277f1c2efd96865872
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-07 16:30:35 +0300

    the task07 is done

--- src/ua/javarush/java/core/level06/task07/Solution.java
+++ src/ua/javarush/java/core/level06/task07/Solution.java
@@ -3,12 +3,11 @@ package ua.javarush.java.core.level06.task07;
 public class Solution {
     public static void main(String[] args) {
         // Оголошуємо змінну й присвоюємо їй ціну товару
-
+        double productPrice = 1234.56789;
 
         // Форматуємо число так, щоб було рівно два знаки після коми
 
-
         // Виводимо відформатоване значення на екран
-
+        System.out.println(String.format("%.2f", productPrice));
     }
 }
