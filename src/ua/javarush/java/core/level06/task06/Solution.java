71b66d1c24f36a6d13c86c3212b8845ee7a42406
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-07 16:17:00 +0300

    started to perform the task06 'Приймання платежу в розумному вендинговому автоматі'

---
+++ src/ua/javarush/java/core/level06/task06/Solution.java
@@ -0,0 +1,19 @@
+package ua.javarush.java.core.level06.task06;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Створюємо об’єкт Scanner для зчитування даних із клавіатури
+
+
+        // Просимо користувача ввести суму платежу
+
+
+        // Зчитуємо введене значення та зберігаємо його у змінній типу double
+
+
+        // Підтверджуємо отриману суму, виводячи її на екран
+
+    }
+}
