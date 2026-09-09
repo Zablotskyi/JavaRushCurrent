0c1a9752725e73ffe9d4c1ff226a57080a727afa
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-09 15:21:14 +0300

    the task11 'Спроба обчислити Невідоме'

---
+++ src/ua/javarush/java/core/level06/task11/Solution.java
@@ -0,0 +1,17 @@
+package ua.javarush.java.core.level06.task11;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Обчислюємо квадратний корінь з -1 за допомогою Math.sqrt
+
+
+        // Виводимо значення mysticAnomaly на екран
+
+
+        // Перевіряємо, чи є mysticAnomaly NaN (Not a Number)
+
+
+        // Виводимо результат перевірки на екран
+
+    }
+}
