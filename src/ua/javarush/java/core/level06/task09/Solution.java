3ac0a2cab7ddc1e220b9386b3b780382f913ea8c
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-09 15:06:24 +0300

    started to perform the task09 'Перевірка точності хімічного розчину'

---
+++ src/ua/javarush/java/core/level06/task09/Solution.java
@@ -0,0 +1,17 @@
+package ua.javarush.java.core.level06.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголошуємо змінну mixtureResult і присвоюємо їй результат додавання 0.1 + 0.2
+
+
+        // Оголошуємо змінну expectedConcentration і присвоюємо їй значення 0.3
+
+
+        // Порівнюємо значення змінних mixtureResult і expectedConcentration за допомогою оператора ==
+
+
+        // Виводимо результат порівняння на екран
+
+    }
+}
