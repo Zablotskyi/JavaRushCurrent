826768d22da11ee63fb33de444e55a56d871a779
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-09 15:14:47 +0300

    started to perform the task10 'Вивчення меж всесвіту обчислень'

---
+++ src/ua/javarush/java/core/level06/task10/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.java.core.level06.task10;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Ділимо 1.0 на 0.0: у світі double це не помилка, а додатна нескінченність
+
+
+        // Виводимо значення змінної (очікується "Infinity")
+
+
+        // Підтверджуємо, що значення справді нескінченність
+
+    }
+}
