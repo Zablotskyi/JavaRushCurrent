5baf9e1d52658dbe5da04c40ae18b1913fd9be33
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-09 15:18:04 +0300

    the task10 is done

--- src/ua/javarush/java/core/level06/task10/Solution.java
+++ src/ua/javarush/java/core/level06/task10/Solution.java
@@ -3,12 +3,12 @@ package ua.javarush.java.core.level06.task10;
 public class Solution {
     public static void main(String[] args) {
         // Ділимо 1.0 на 0.0: у світі double це не помилка, а додатна нескінченність
-
+        double cosmicEvent = 1.0 / 0.0;
 
         // Виводимо значення змінної (очікується "Infinity")
-
+        System.out.println(cosmicEvent);
 
         // Підтверджуємо, що значення справді нескінченність
-
+        System.out.println(Double.isInfinite(cosmicEvent));
     }
 }
