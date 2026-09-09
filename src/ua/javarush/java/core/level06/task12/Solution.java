33b82df49d435d616100cc3d90ef73fa93dff31c
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-09 15:23:02 +0300

    started to perform the task12 'Калібрування високоточного лазера'

---
+++ src/ua/javarush/java/core/level06/task12/Solution.java
@@ -0,0 +1,20 @@
+package ua.javarush.java.core.level06.task12;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Оголошуємо змінну actualFrequency і присвоюємо їй значення 0.1 + 0.2
+
+
+        // Оголошуємо змінну targetFrequency і присвоюємо їй значення 0.3
+
+
+        // Оголошуємо змінну tolerance і задаємо їй значення 0.000001
+
+
+        // Обчислюємо абсолютну різницю між actualFrequency і targetFrequency
+
+
+        // Порівнюємо різницю з допуском і виводимо відповідне повідомлення
+
+    }
+}
