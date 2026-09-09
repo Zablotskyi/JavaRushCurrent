3cb37afcb59f7c05baaa8e47dc8800d014081a1b
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-09 15:22:06 +0300

    the task11 is done

--- src/ua/javarush/java/core/level06/task11/Solution.java
+++ src/ua/javarush/java/core/level06/task11/Solution.java
@@ -3,15 +3,15 @@ package ua.javarush.java.core.level06.task11;
 public class Solution {
     public static void main(String[] args) {
         // Обчислюємо квадратний корінь з -1 за допомогою Math.sqrt
-
+        double mysticAnomaly = Math.sqrt(-1);
 
         // Виводимо значення mysticAnomaly на екран
-
+        System.out.println(mysticAnomaly);
 
         // Перевіряємо, чи є mysticAnomaly NaN (Not a Number)
-
+        boolean result = Double.isNaN(mysticAnomaly);
 
         // Виводимо результат перевірки на екран
-
+        System.out.println(result);
     }
 }
