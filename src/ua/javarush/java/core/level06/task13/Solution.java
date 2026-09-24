8ba0531c94c9d8cbc94996b974c8043f823216d8
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-24 15:52:46 +0300

    the task13 is done

--- src/ua/javarush/java/core/level06/task13/Solution.java
+++ src/ua/javarush/java/core/level06/task13/Solution.java
@@ -3,12 +3,12 @@ package ua.javarush.java.core.level06.task13;
 public class Solution {
     public static void main(String[] args) {
         // Початкова оцінка кількості металу (у тоннах) із дробовою частиною
-
+        double rawMetalEstimate = 789.456;
 
         // Округлюємо до найближчого цілого числа за допомогою Math.round (результат типу long)
-
+        int newrawMetalEstimate = (int) Math.round(rawMetalEstimate);
 
         // Виводимо округлене значення на екран
-
+        System.out.println(newrawMetalEstimate);
     }
 }
