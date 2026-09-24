3cb9ddc010c9c3ce5c2c6c9991a1f9ea7efc0de1
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-24 15:28:59 +0300

    started to perform the task 'Оцінка ресурсів для космічної колонії'

---
+++ src/ua/javarush/java/core/level06/task13/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.java.core.level06.task13;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Початкова оцінка кількості металу (у тоннах) із дробовою частиною
+
+
+        // Округлюємо до найближчого цілого числа за допомогою Math.round (результат типу long)
+
+
+        // Виводимо округлене значення на екран
+
+    }
+}
