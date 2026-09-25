00d384bcacaec8c78c4196a4adfff509af2f3908
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-25 13:47:08 +0300

    the task14 is done

--- src/ua/javarush/java/core/level06/task14/Solution.java
+++ src/ua/javarush/java/core/level06/task14/Solution.java
@@ -3,13 +3,13 @@ package ua.javarush.java.core.level06.task14;
 public class Solution {
     public static void main(String[] args) {
         // Початкова ціна еліксиру з податком
-
+        double elixirPriceWithTax = 17.34567;
 
         // Округлюємо до двох знаків після коми:
         // Множимо на 100, округлюємо за допомогою Math.round, потім ділимо на 100.0
-
+        double displayPrice = Math.round(elixirPriceWithTax * 100) / 100.0;
 
         // Виводимо округлену ціну на екран
-
+        System.out.println(displayPrice);
     }
 }
