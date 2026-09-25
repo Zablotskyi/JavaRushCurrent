2864532e6b6437970b66b197fd61da6f32bc7eed
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-09-25 13:41:54 +0300

    started to perform the task14 'Точний розрахунок вартості еліксиру з податком'

---
+++ src/ua/javarush/java/core/level06/task14/Solution.java
@@ -0,0 +1,15 @@
+package ua.javarush.java.core.level06.task14;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Початкова ціна еліксиру з податком
+
+
+        // Округлюємо до двох знаків після коми:
+        // Множимо на 100, округлюємо за допомогою Math.round, потім ділимо на 100.0
+
+
+        // Виводимо округлену ціну на екран
+
+    }
+}
